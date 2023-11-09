4ec5163339cd9bbcc026878b0d41b9426507c251
Sophakabuso <60679360@mylife.unisa.ac.za>
2023-11-09 21:58:26 +0200

    test

--- src/services/geolocation.js
+++ src/services/geolocation.js
@@ -1,51 +1,36 @@
-import Geolocation from 'react-native-geolocation-service';
-import SendSMS from 'react-native-sms';
-import { check, PERMISSIONS, RESULTS, request } from 'react-native-permissions';
+import * as Location from 'expo-location';
+import * as SMS from 'expo-sms';
 
 // Function to get the user's real-time GPS coordinates
-const getCoordinates = () => {
-  return new Promise((resolve, reject) => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        resolve(position);
-      },
-      (error) => {
-        reject(error);
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  });
+const getCoordinates = async () => {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== 'granted') {
+    throw new Error('Permission to access location was denied');
+  }
+
+  let location = await Location.getCurrentPositionAsync({});
+  return location;
 };
 
 // Function to send an SMS with the user's coordinates
-const sendCoordinates = async (phoneNumber, message) => {
+const sendCoordinates = async (phoneNumber) => {
   try {
     const position = await getCoordinates();
     const message = `My current location is: https://www.google.com/maps/?q=${position.coords.latitude},${position.coords.longitude}`;
-    SendSMS.send(
-      {
-        body: message,
-        recipients: [phoneNumber],
-        successTypes: ['sent', 'queued'],
-      },
-      (completed, cancelled, error) => {
-        console.log('SMS Callback: completed: ' + completed + ' cancelled: ' + cancelled + 'error: ' + error);
-      }
-    );
+
+    const isAvailable = await SMS.isAvailableAsync();
+    if (isAvailable) {
+      const { result } = await SMS.sendSMSAsync(
+        [phoneNumber],
+        message
+      );
+      console.log(result);
+    } else {
+      throw new Error('SMS service not available');
+    }
   } catch (error) {
     console.error('Error getting coordinates: ', error);
   }
 };
 
-// Function to request the necessary permissions
-const requestPermissions = async () => {
-  const locationPermission = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-  if (locationPermission !== RESULTS.GRANTED) {
-    await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
-  }
-  const smsPermission = await check(PERMISSIONS.ANDROID.SEND_SMS);
-  if (smsPermission !== RESULTS.GRANTED) {
-    await request(PERMISSIONS.ANDROID.SEND_SMS);
-  }
-};
-export { sendCoordinates, requestPermissions };
+export { sendCoordinates };
